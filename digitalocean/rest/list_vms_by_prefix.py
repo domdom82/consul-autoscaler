d25b401ebdbbd56090ec33d619efd46a6e7703c4
Dominik Jall <djall@de.ibm.com>
2016-10-03 18:31:12 +0200

    added list_vms_by_prefix.py

---
+++ digitalocean/rest/list_vms_by_prefix.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# Returns a vm ip for a VM name
+
+import sys
+import os
+import json
+import subprocess
+import re
+
+if (len(sys.argv) != 2):
+  print "Usage: list_vms_by_prefix prefix"
+  exit(-1);
+
+MYDIR=os.path.abspath(os.path.dirname(sys.argv[0]))
+HOSTNAME=sys.argv[1]
+
+# Grab all VMs
+def getAllVMs():
+  cmd = [ '/bin/sh', MYDIR + '/list_vms.sh' ]
+  output = subprocess.check_output(cmd)
+  vms = json.loads(output)['droplets']
+  return vms
+
+# get the VMs filtered by prefix
+def getPrefixVMs(vms, prefix):
+  def prefixFilter(vm):
+    pattern = "(\w+)-(\w+)"
+    result = re.match(pattern, vm['name'])
+    if result != None:
+      vmprefix = result.group(1)
+      if vmprefix == prefix:
+        return True
+    return False
+
+  return filter(prefixFilter, vms)
+
+vms = getAllVMs()
+prefix_vms = getPrefixVMs(vms, HOSTNAME)
+print json.dumps(prefix_vms)
