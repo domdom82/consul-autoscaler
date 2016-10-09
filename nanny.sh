2d631fc00912e948485a7a944db9fabcd5d2be7d
Dominik Jall <djall@de.ibm.com>
2016-10-09 23:54:48 +0200

    debug output

--- nanny.sh
+++ nanny.sh
@@ -3,7 +3,10 @@ set -e
 
 SCRIPTDIR="$(cd $(dirname "$0")/ && pwd)"
 
-echo "AUTOSCALER_TYPE == $AUTOSCALER_TYPE"
+echo "nanny.sh start at $(date)" >> /logs/nanny.log
 
 cd $SCRIPTDIR
-./nanny.py $AUTOSCALER_TYPE
+./nanny.py $AUTOSCALER_TYPE >> /logs/nanny.log
+
+echo "nanny.sh stop at $(date)" >> /logs/nanny.log
+echo "\n\n" >> /logs/nanny.log
